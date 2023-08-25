00ab45c8439a8cee4d241e4455bb33743588d559
Gorad Tai Laxman <121398830+taigorad@users.noreply.github.com>
2023-08-25 15:38:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const cors=require("cors")
 const mongo2=require("./mongo-connection/mongo2")
 app.use(cors())
  // this is for the build connection 
-app.use(express.static(path.join(__dirname,"../client/build")))
+app.use(express.static(path.join(__dirname,"./client/build")))
 app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname,"../client/build/index.html"))
+res.sendFile(path.join(__dirname,"./client/build/index.html"))
 })
 
 const mongo1=require("./mongo-connection/mongo1")
