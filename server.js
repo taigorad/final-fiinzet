3145941eeb96a5487a2dcc27299d5ac3a3034565
Gorad Tai Laxman <121398830+taigorad@users.noreply.github.com>
2023-08-30 10:04:58 +0530

    ajit amane mail

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.post("/contact",async(req,res)=>{
       const mailoption={
     
       from:"ajit.amane1901@gmail.com",
-    //  to:"ajit.amane@finzet.com",
-    to:"priyapkadam1182@gmail.com",
+      to:"ajit.amane@fiinzet.com",
+   // to:"priyapkadam1182@gmail.com",
     //to:"ajit.amane1901@gmail.com",
       subject:"Fiinzet : User Contact Details ",
      // text:JSON.stringify(result1)
@@ -92,7 +92,8 @@ let result1=await result.save()
   
     const mailoption={
       from:"ajit.amane1901@gmail.com",
-      to:"priyapkadam1182@gmail.com",
+      //to:"priyapkadam1182@gmail.com",
+      to:"ajit.amane@fiinzet.com",
    // to:"ajit.amane1901@gmail.com",
     subject:"Fiinzet : User Contact Details",
     text:`Full_Name:${result1.name},
