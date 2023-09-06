3079cc4c712b9f06ffae39d995e696cc5145952b
Gorad Tai Laxman <121398830+taigorad@users.noreply.github.com>
2023-09-06 15:03:27 +0530

    Update mongo.js

--- mongo-connection/mongo.js
+++ mongo-connection/mongo.js
@@ -1,10 +1,10 @@
 const mongoo=require("mongoose")
 
 
-//const URL="mongodb+srv://taigorad:goradwadi10@cluster0.ba6s9tj.mongodb.net/finjet"
+const URL="mongodb+srv://taigorad:goradwadi10@cluster0.ba6s9tj.mongodb.net/finjet"
 //const URL="mongodb+srv://brand_liberty:brandliberty10@cluster0.mfvfsxn.mongodb.net//finjet"
 //const URL="mongodb+srv://brand_liberty:brandliberty10@cluster0.mfvfsxn.mongodb.net/finjet"
-const URL="mongodb+srv://brand_liberty_web:brandliberty1010@cluster0.wqfp3cw.mongodb.net/finjet"
+//const URL="mongodb+srv://brand_liberty_web:brandliberty1010@cluster0.wqfp3cw.mongodb.net/finjet"
 const congfig={
     useNewUrlParser:true,
     useUnifiedTopology:true
